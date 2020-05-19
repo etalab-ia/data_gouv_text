7b76b5b6bdb30bccc8cd17742e2e2e16b711171d
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-05-19 19:40:45 +0200

    added vars to bash

--- get_datagouv_texts.sh
+++ get_datagouv_texts.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+RESOURCE_CSV="./data/resources.csv"
+DOWNLOAD_OUTPUT="./output/"
+FILE_TYPE="pdf"
+TXT_OUTPUT="./output"
 
 eval "$(conda shell.bash hook)"
 conda activate data_gouv_text
 
+
 echo "Download pdf files"
-python src/download_files.py output output
+python src/download_files.py $RESOURCE_CSV $DOWNLOAD_OUTPUT $FILE_TYPE
 
 echo "Transform to text"
-python src/pdf2txt.py output output
+python src/pdf2txt.py $DOWNLOAD_OUTPUT $TXT_OUTPUT
